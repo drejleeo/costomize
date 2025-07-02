4fa71760c2d941dc35931c2003ecd93bd3acd632
Andrei <41676929+drejleeo@users.noreply.github.com>
2025-07-02 16:01:13 +0300

    Add hammerspoon

--- macos/programs.sh
+++ macos/programs.sh
@@ -1,2 +1,12 @@
-# Install programs
+# Programs
 brew install --cask vscodium
+
+# Multi-line installations
+install_hammerspoon () {
+  brew install hammerspoon
+  mkdir ~/.hammerspoon
+  curl https://raw.githubusercontent.com/drejleeo/costomize/refs/heads/main/wifi/init.lua > ~/.hammerspoon/init.lua
+}
+
+# Tools
+install_hammerspoon
