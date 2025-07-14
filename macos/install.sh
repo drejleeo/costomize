40b600489ac65080a5f2a05efe7ba6ffc908feed
Andrei <41676929+drejleeo@users.noreply.github.com>
2025-07-14 17:55:28 +0300

    Update install.sh

--- macos/install.sh
+++ macos/install.sh
@@ -1,12 +1,15 @@
-# Programs
-brew install --cask vscodium
-brew install hammerspoon
-
 # Multi-line installations
 configure_hammerspoon () {
   mkdir ~/.hammerspoon
   curl https://raw.githubusercontent.com/drejleeo/costomize/refs/heads/main/macos/scripts/init.lua > ~/.hammerspoon/init.lua
 }
 
-# Run configurations
-configure_hammerspoon
+configure_podman () {
+  podman machine init
+  podman machine start
+}
+
+# Programs
+brew install --cask vscodium
+brew install hammerspoon && configure_hammerspoon
+brew install podman && configure_podman
